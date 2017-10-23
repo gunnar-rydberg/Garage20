74748371b790cd2351ca25ca5863499e23ebf38d
User <User@DESK-CLZ7J32>
2017-10-23 10:25:48 +0200

    create model

---
+++ Garage20/Models/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage20.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string RegNo { get; set; }
+        public string Color { get; set; }
+        public int NoWheels { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
