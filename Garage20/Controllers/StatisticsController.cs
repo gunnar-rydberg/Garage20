048786604c7d14b2784bd470155719dcf3015830
Thomas Palestig <tpalestig@gmail.com>
2017-10-25 15:05:51 +0200

    Add statistics page Add statistics controller Add Statistics in Menu Add Images/Icons

---
+++ Garage20/Controllers/StatisticsController.cs
@@ -0,0 +1,47 @@
+﻿using Garage20.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Garage20.Utility;
+
+namespace Garage20.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private Garage20Context db = new Garage20Context();
+
+        // GET: Statistics
+        public ActionResult Index()
+        {
+            Statistics stats = new Statistics();
+
+            stats.Wheels = db.Vehicles.Sum(v => v.NoWheels);
+
+
+            var dates = db.Vehicles.Select(v => v.Date);
+            DateTime now = DateTime.Now;
+
+            foreach (var d in dates)
+            {
+                stats.Time += now - d;
+            }
+
+
+            var vehicleTypes = db.Vehicles.Select(v => v.Type).Distinct();
+
+            foreach (var t in vehicleTypes)
+            {
+                int n = db.Vehicles.Count(v => v.Type == t);
+                stats.Types.Add(t.ToString(), n);
+            }
+
+
+            stats.Price = (int)Math.Ceiling(stats.Time.TotalHours) * CalculatPrice.HOURLY_PRICE;
+
+
+            return View(stats);
+        }
+    }
+}
---
+++ Garage20/Models/Statistics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Garage20.Models
+{
+    public class Statistics
+    {
+        public Dictionary<string, int> Types { get; set; } = new Dictionary<string, int>();
+        public int Wheels { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:%d} day(s) {0:hh\\:mm\\:ss}")]
+        public TimeSpan Time { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+    }
+}
